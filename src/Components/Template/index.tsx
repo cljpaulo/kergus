7d0ccee45e6a6e3baa2414db4ec853cde2c50bd6
Paulo César Lattaro Jacob <cljpaulo99@gmail.com>
2023-12-29 11:40:03 -0300

    💄 style: Template com nova estilização

--- src/Components/Template/index.tsx
+++ src/Components/Template/index.tsx
@@ -1,4 +1,7 @@
+import { Foundation } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
 import Dashboard from '../../Screens/Dashboard';
 import Home from '../../Screens/Home';
 import Profile from '../../Screens/Profile';
@@ -7,10 +10,50 @@ const Tab = createBottomTabNavigator();
 
 export default function Template() {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-            <Tab.Screen name="Dashboard" component={Dashboard} />
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: '#FF8C00',
+                tabBarInactiveTintColor: 'white',
+                tabBarStyle: {
+                    backgroundColor: '#0B0B81',
+                    height: 60,
+                },
+                tabBarLabelStyle: {
+                    display: 'none',
+                },
+                tabBarIconStyle: {
+                    marginTop: 5,
+                },
+            }}>
+            <Tab.Screen
+                name="Dashboard"
+                component={Dashboard}
+                options={{
+                    tabBarIcon: ({ color }: any) => (
+                        <Foundation name="graph-pie" size={RFValue(30)} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color }: any) => (
+                        <Foundation name="home" size={RFValue(30)} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarIcon: ({ color }: any) => (
+                        <Foundation name="torso" size={RFValue(30)} color={color} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
