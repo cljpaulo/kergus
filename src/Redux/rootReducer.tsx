62ac747f0106aafe291e39812e7e27812fff3320
Paulo César Lattaro Jacob <cljpaulo99@gmail.com>
2024-02-06 22:14:35 -0300

    ✨ feat: registro atual selecionado no redux

--- src/Redux/rootReducer.tsx
+++ src/Redux/rootReducer.tsx
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import currentStageReducer from './Current-Stage/index';
+import currentValueReducer from './Current-Value/index';
 
 const rootReducer = combineReducers({
     currentStage: currentStageReducer,
+    currentValue: currentValueReducer,
 });
 
 export default rootReducer;
